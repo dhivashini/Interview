92935d55a44ffc95abde285e3ce4a4d6ec59ac7c
crosspitter <dhiva33@gmail.com>
2016-10-05 19:15:28 -0700

    stack

--- src/com/dhiva/implementation/StackUsage.java
+++ src/com/dhiva/implementation/StackUsage.java
@@ -1,5 +1,19 @@
 package com.dhiva.implementation;
+import java.util.Stack;
 
 public class StackUsage {
+	public static void main(String[] args){
+		Stack s = new Stack();
+		s.push(new Integer(3));
+		s.push(new Integer(6));
+		s.push(new Integer(8));
+		System.out.println(s.size());
+		System.out.println(s);
+		s.pop();
+		System.out.println(s);
+		s.pop();
+		System.out.println(s);
 
+		
+	}
 }
