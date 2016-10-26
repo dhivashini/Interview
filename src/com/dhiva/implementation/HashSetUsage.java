1c7c51dc3e62d211720fd9569961ef16f7cd8603
crosspitter <dhiva33@gmail.com>
2016-10-26 14:55:32 -0700

    compareTo and comparable

--- src/com/dhiva/implementation/HashSetUsage.java
+++ src/com/dhiva/implementation/HashSetUsage.java
@@ -1,15 +1,19 @@
 package com.dhiva.implementation;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 
 public class HashSetUsage {
 	public static void main(String[] args) {
 		Person dhiva = new Person("dhisha", "jagan", "f", 20);
 		Person hari = new Person("dhisha", "jagan", "f", 20);
-		Person hani = new Person("hanumesh", "m", "m", 24);
 		Person sk = new Person("kou", "sekhar", "m", 24);
 		Person manoj = new Person("manash", "ravi", "m", 24);
+		Person hani = new Person("hanumesh", "m", "m", 24);
 
 		HashSet<Person> setObj = new HashSet<Person>();
 		// add objects
@@ -21,7 +25,7 @@ public class HashSetUsage {
 		// check if set is empty
 		System.out.println("is set empty? " + setObj.isEmpty());
 		// remove object
-		setObj.remove(3);
+		// setObj.remove(sk);
 		// iterate the set
 		Iterator setIterator = setObj.iterator();
 		while (setIterator.hasNext()) {
@@ -32,14 +36,37 @@ public class HashSetUsage {
 			System.out.printf(p.getFirstName(), p.getLastName());
 			System.out.println();
 		}
-		//check for equals method
+		// check for equals method
 		System.out.println(dhiva.equals(hari));
 		System.out.println(dhiva.hashCode());
 		System.out.println(hari.hashCode());
+		List<Person> sortedList = new ArrayList(setObj);
+
+		// compareTo
+
+		Collections.sort(sortedList, new Comparator<Person>() {
+			@Override
+			public int compare(Person o1, Person o2) {
+				return o1.getFirstName().compareTo(o2.getFirstName());
+			}
+		});
+
+		for (Person p : sortedList) {
+			System.out.printf(p.getFirstName(), p.getLastName());
+			System.out.println();
+		}
+
+		// comparable
+		Collections.sort(sortedList);
+		for (Person p : sortedList) {
+			System.out.println(p.getAge());
+
+		}
 		// convert hashset to array
 		Object[] hashsetArray = setObj.toArray();
 		for (Object i : hashsetArray) {
 			System.out.println(i);
 		}
 	}
+
 }
--- src/com/dhiva/implementation/Person.java
+++ src/com/dhiva/implementation/Person.java
@@ -1,6 +1,6 @@
 package com.dhiva.implementation;
 
-public class Person {
+public class Person implements Comparable {
 	public String firstName, lastName, sex;
 	public int age;
 
@@ -14,16 +14,26 @@ public class Person {
 		this.sex = sex;
 		this.age = age;
 	}
-	public String getFirstName(){
+
+	public String getFirstName() {
 		return this.firstName;
 	}
-	public String getLastName(){
+
+	public String getLastName() {
 		return this.firstName;
 	}
-	public String sex(){
+
+	public String getSex() {
 		return this.sex;
 	}
-	public int age(){
+
+	public int getAge() {
 		return this.age;
+	}
+
+	@Override
+	public int compareTo(Object arg0) {
+		Person p1 = (Person) arg0;
+		return (this.age < p1.age) ? -1 : (this.age > p1.age) ? 1 : 0;
 	}
 }
