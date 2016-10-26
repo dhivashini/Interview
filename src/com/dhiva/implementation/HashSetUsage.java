96d7ee91fa723bef2a26e2aa023f987b7e4ce196
crosspitter <dhiva33@gmail.com>
2016-10-26 12:05:46 -0700

    person class

--- src/com/dhiva/implementation/HashSetUsage.java
+++ src/com/dhiva/implementation/HashSetUsage.java
@@ -5,12 +5,19 @@ import java.util.Iterator;
 
 public class HashSetUsage {
 	public static void main(String[] args) {
-		HashSet<Integer> setObj = new HashSet<Integer>();
+		Person dhiva = new Person("dhisha", "jagan", "f", 20);
+		Person hari = new Person("dhisha", "jagan", "f", 20);
+		Person hani = new Person("hanumesh", "m", "m", 24);
+		Person sk = new Person("kou", "sekhar", "m", 24);
+		Person manoj = new Person("manash", "ravi", "m", 24);
+
+		HashSet<Person> setObj = new HashSet<Person>();
 		// add objects
-		setObj.add(2);
-		setObj.add(3);
-		setObj.add(4);
-		setObj.add(5);
+		setObj.add(dhiva);
+		setObj.add(hari);
+		setObj.add(hani);
+		setObj.add(sk);
+		setObj.add(manoj);
 		// check if set is empty
 		System.out.println("is set empty? " + setObj.isEmpty());
 		// remove object
@@ -20,9 +27,18 @@ public class HashSetUsage {
 		while (setIterator.hasNext()) {
 			System.out.println(setIterator.next());
 		}
+		// get person info
+		for (Person p : setObj) {
+			System.out.printf(p.getFirstName(), p.getLastName());
+			System.out.println();
+		}
+		//check for equals method
+		System.out.println(dhiva.equals(hari));
+		System.out.println(dhiva.hashCode());
+		System.out.println(hari.hashCode());
 		// convert hashset to array
 		Object[] hashsetArray = setObj.toArray();
-		for(Object i :hashsetArray){
+		for (Object i : hashsetArray) {
 			System.out.println(i);
 		}
 	}
--- src/com/dhiva/implementation/Person.java
+++ src/com/dhiva/implementation/Person.java
@@ -1,18 +1,29 @@
 package com.dhiva.implementation;
 
 public class Person {
-	public String firstName, lastName;
-	public int salary, age;
+	public String firstName, lastName, sex;
+	public int age;
 
 	Person() {
 
 	}
 
-	public Person(String firstName, String lastName, int salary, int age) {
+	public Person(String firstName, String lastName, String sex, int age) {
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.salary = salary;
+		this.sex = sex;
 		this.age = age;
 	}
-
+	public String getFirstName(){
+		return this.firstName;
+	}
+	public String getLastName(){
+		return this.firstName;
+	}
+	public String sex(){
+		return this.sex;
+	}
+	public int age(){
+		return this.age;
+	}
 }
