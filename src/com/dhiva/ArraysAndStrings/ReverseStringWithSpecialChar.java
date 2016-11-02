8c7ab94968d4777d63d1e56f4e5b72e273421b79
crosspitter <dhiva33@gmail.com>
2016-11-01 21:32:21 -0700

    arraylist implementation test

--- src/com/dhiva/ArraysAndStrings/ReverseStringWithSpecialChar.java
+++ src/com/dhiva/ArraysAndStrings/ReverseStringWithSpecialChar.java
@@ -1,16 +1,16 @@
 package com.dhiva.ArraysAndStrings;
 
 public class ReverseStringWithSpecialChar {
-	public static String reverseString(String input){
-		char[] inputCharArray = input.toCharArray();
-		char[] output;
-		for(int i=0;i<input.length();i++){
-			if(input.charAt(i)<Character.valueOf('a')||input.charAt(i)<Character.valueOf('z')){
-				output[i]=input.charAt(i);
-			}
-		}
-		
-		for(int i=0;i<)
-		return output;
-	}
+//	public static String reverseString(String input){
+//		char[] inputCharArray = input.toCharArray();
+//		char[] output;
+//		for(int i=0;i<input.length();i++){
+//			if(input.charAt(i)<Character.valueOf('a')||input.charAt(i)<Character.valueOf('z')){
+//				output[i]=input.charAt(i);
+//			}
+//		}
+//		
+////		for(int i=0;i<)
+////		return output;
+//	}
 }
---
+++ tests/com/dhiva/implementation/ArrayListImplementationTest.java
@@ -0,0 +1,20 @@
+package com.dhiva.implementation;
+
+import org.junit.Test;
+
+public class ArrayListImplementationTest {
+	@Test
+	public void test(){
+		ArrayListImplementation arrList = new ArrayListImplementation();
+		arrList.add(2);
+		arrList.get(3);
+		arrList.add(4);
+		arrList.add("dhiva");
+		arrList.add("hi");
+		arrList.size();
+		arrList.remove(1);
+		
+
+	}
+	
+}
