ff71aa329cabd15b3f6106c7cce650be0ba166eb
crosspitter <dhiva33@gmail.com>
2016-11-04 18:27:32 -0700

    remove duplicate in ll

--- src/com/dhiva/linkedlist/RemoveDuplicates.java
+++ src/com/dhiva/linkedlist/RemoveDuplicates.java
@@ -1,19 +1,24 @@
 package com.dhiva.linkedlist;
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class RemoveDuplicates {
  public static void removeDups(LinkedListNode n){
 	 HashSet<Integer> set = new HashSet<Integer>();
 	 LinkedListNode previous = null;
 	 while(n!=null){
-//		 if(n.value){
-//			 
-//		 }else{
-//			 set.add(n.value);
-//			 previous=n;
-//		 }
+		 if(set.contains(n.value)){
+			 previous.next=n.next;
+		 }else{
+			 set.add(n.value);
+			 previous=n;
+		 }
 		 n=n.next;
 	 }
+	 Iterator<Integer> i= set.iterator();
+	 while(i.hasNext())
+		 System.out.println(i.next());
  }
+ 	
 }
---
+++ tests/com/dhiva/linkedlistetsts/RemoveDuplicatesTest.java
@@ -0,0 +1,23 @@
+package com.dhiva.linkedlistetsts;
+
+import static org.junit.Assert.assertEquals;
+
+
+
+import org.junit.Test;
+
+import com.dhiva.linkedlist.LinkedListNode;
+import com.dhiva.linkedlist.RemoveDuplicates;
+
+public class RemoveDuplicatesTest {
+	@Test
+	public void test() {
+		LinkedListNode n1 = new LinkedListNode(20, null);
+		LinkedListNode n2 = new LinkedListNode(21, n1);
+		LinkedListNode n3 = new LinkedListNode(2, n2);
+		LinkedListNode n4 = new LinkedListNode(21, n3);
+		LinkedListNode n5 = new LinkedListNode(24, n4);
+		LinkedListNode n6 = new LinkedListNode(26, n5);
+		RemoveDuplicates.removeDups(n6);
+	}
+}
