fe5553bdde52281d7f6f53fb2d8b0948e8a36adb
Dhivashini Jaganathan <dhivashini.jaganathan@sjsu.edu>
2017-03-08 17:48:35 -0800

    valid sudoku

--- src/com/dhiva/ArraysAndStrings/ValidSudoku.java
+++ src/com/dhiva/ArraysAndStrings/ValidSudoku.java
@@ -3,29 +3,29 @@ package com.dhiva.ArraysAndStrings;
 import java.util.HashSet;
 
 public class ValidSudoku {
-	public boolean isValid(char[][] board){
-		
-		HashSet<Character> rows = new HashSet<Character>();
-        HashSet<Character> columns = new HashSet<Character>();
-        HashSet<Character> cube = new HashSet<Character>();
+	public boolean isValid(char[][] board){	
         for(int i=0;i<9;i++){
+        	HashSet<Character> rows = new HashSet<Character>();
+            HashSet<Character> columns = new HashSet<Character>();
         	for(int j=0;j<9;j++){
         	if(!(board[i][j]=='.')&& !(rows.add(board[i][j])))
         		return false;
         	if(!(board[i][j]=='.')&& !(columns.add(board[i][j])))
         		return false;
-        	if( !UsedInBox(board, i - i%3 , j - j%3,cube))
-        		return false;
+       	
         	}
         }
-		return true;
+        for(int block = 0 ;block<9;block++){
+            HashSet<Character> cube = new HashSet<Character>();
+            for (int i = block / 3 * 3; i < block / 3 * 3 + 3; i++) {
+    			for (int j = block % 3 * 3; j < block % 3 * 3 + 3; j++) {
+    				if(!(board[i][j]=='.') && !(cube.add(board[i][j])))
+    						return false;
+    			}
+        }
 	}
+	
+    return true;
 
-	private boolean UsedInBox(char[][] board, int boxStartRow, int boxStartCol,HashSet<Character> cube) {
-		for (int row = 0; row < 3; row++)
-			for (int col = 0; col < 3; col++)
-				if (!cube.add(board[row+boxStartRow] [col+boxStartCol]))
-					return false;
-		return true;	
 	}
 }
