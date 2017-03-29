97953a1aa844e4193ddb1892351b1adabeeff90e
Dhivashini Jaganathan <dhivashini.jaganathan@sjsu.edu>
2017-03-28 19:24:16 -0700

    combination sum

--- src/com/dhiva/ArraysAndStrings/CombinationSum.java
+++ src/com/dhiva/ArraysAndStrings/CombinationSum.java
@@ -1,5 +1,28 @@
 package com.dhiva.ArraysAndStrings;
 
-public class CombinationSum {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class CombinationSum {
+	public List<List<Integer>> findSum(int[] nums, int target) {
+		List<List<Integer>> list = new ArrayList<>();
+		Arrays.sort(nums);
+		backtrack(list, new ArrayList<>(), nums, target, 0);
+		return list;
+	}
+
+	private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
+		if (remain < 0)
+			return;
+		else if (remain == 0)
+			list.add(new ArrayList<>(tempList));
+		else {
+			for (int i = start; i < nums.length; i++) {
+				tempList.add(nums[i]);
+				backtrack(list, tempList, nums, remain - nums[i], i);
+				tempList.remove(tempList.size() - 1);
+			}
+		}
+	}
 }
