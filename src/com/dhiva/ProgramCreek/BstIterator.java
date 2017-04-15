5ac0387de66e5256b5f3d56ac31694bab907644e
Dhivashini Jaganathan <dhivashini.jaganathan@sjsu.edu>
2017-04-15 11:41:49 -0700

    Bst Iterator

--- src/com/dhiva/ProgramCreek/BstIterator.java
+++ src/com/dhiva/ProgramCreek/BstIterator.java
@@ -1,5 +1,33 @@
 package com.dhiva.ProgramCreek;
 
-public class BstIterator {
+import java.util.Stack;
 
+import com.dhiva.problems_a.TreeNode;
+
+public class BstIterator {
+	Stack<TreeNode> stack;
+
+	public BstIterator(TreeNode root) {
+		stack = new Stack<>();
+		while (root != null) {
+			stack.push(root);
+			root = root.left;
+		}
+	}
+
+	public boolean hasNext() {
+		return !stack.isEmpty();
+	}
+
+	public int next() {
+		TreeNode result = stack.pop();
+		if (result.right != null) {
+			TreeNode curr = result.right;
+			while (curr != null) {
+				stack.push(curr);
+				curr = curr.left;
+			}
+		}
+		return result.data;
+	}
 }
