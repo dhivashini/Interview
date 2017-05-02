9078d4a1d419123cf716fc942cae170511791605
Dhivashini Jaganathan <dhivashini.jaganathan@sjsu.edu>
2017-05-02 12:03:37 -0700

    Unique bst

--- src/com/dhiva/ProgramCreek/UniqueBst.java
+++ src/com/dhiva/ProgramCreek/UniqueBst.java
@@ -1,5 +1,14 @@
 package com.dhiva.ProgramCreek;
 
 public class UniqueBst {
-
+	public int findUniqueBst(int[] input) {
+		int[] arr = new int[input.length + 1];
+		arr[0] = arr[1] = 1;
+		for (int i = 2; i <= input.length; i++) {
+			for (int j = 1; j <= i; j++) {
+				arr[i] += arr[j - 1] * arr[i - j];
+			}
+		}
+		return arr[input.length];
+	}
 }
