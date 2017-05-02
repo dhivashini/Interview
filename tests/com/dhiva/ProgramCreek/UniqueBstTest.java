2be607af62a1ae9fc51bbb69e90c0dc7b1bac371
Dhivashini Jaganathan <dhivashini.jaganathan@sjsu.edu>
2017-05-02 12:03:57 -0700

    bst test

--- tests/com/dhiva/ProgramCreek/UniqueBstTest.java
+++ tests/com/dhiva/ProgramCreek/UniqueBstTest.java
@@ -1,5 +1,15 @@
 package com.dhiva.ProgramCreek;
 
-public class UniqueBstTest {
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+public class UniqueBstTest {
+	@Test
+	public void test() {
+		UniqueBst obj = new UniqueBst();
+		int[] input = { 1, 2, 3 };
+		int output = obj.findUniqueBst(input);
+		assertEquals(5, output);
+	}
 }
