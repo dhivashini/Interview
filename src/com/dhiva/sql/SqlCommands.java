3c67585e1d8707a7141c8f5768030c8a1c4fc23b
Dhivashini Jaganathan <dhivashini.jaganathan@sjsu.edu>
2017-05-09 22:24:10 -0700

    rest

--- src/com/dhiva/sql/SqlCommands.java
+++ src/com/dhiva/sql/SqlCommands.java
@@ -1,5 +1,69 @@
 package com.dhiva.sql;
+/*
+JOINS
+(INNER) JOIN: Returns records that have matching values in both tables
+LEFT (OUTER) JOIN: Return all records from the left table, and the matched records from the right table
+RIGHT (OUTER) JOIN: Return all records from the right table, and the matched records from the left table
+FULL (OUTER) JOIN: Return all records when there is a match in either left or right table
 
-public class SqlCommands {
+SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
+FROM Orders
+FULL JOIN Customers
+ON Orders.CustomerID=Customers.CustomerID;
 
-}
+
+SELECT - extracts data from a database
+UPDATE - updates data in a database
+DELETE - deletes data from a database
+INSERT INTO - inserts new data into a database
+CREATE DATABASE - creates a new database
+ALTER DATABASE - modifies a database
+CREATE TABLE - creates a new table
+ALTER TABLE - modifies a table
+DROP TABLE - deletes a table
+CREATE INDEX - creates an index (search key)
+DROP INDEX - deletes an index
+
+SELECT * FROM Customers
+WHERE Country='Mexico';
+
+SELECT * FROM Customers
+WHERE CustomerID=1;
+
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition1 AND condition2 AND condition3 ...;
+
+SELECT column1, column2, ...
+FROM table_name
+ORDER BY column1, column2, ... ASC|DESC;
+
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);
+
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition;
+
+SELECT column_name(s)
+FROM table_name
+WHERE column_name IN (SELECT STATEMENT);
+
+SELECT column_name(s)
+FROM table_name
+WHERE condition
+GROUP BY column_name(s)
+ORDER BY column_name(s);
+
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country
+HAVING COUNT(CustomerID) > 5;
+
+Resources expose easily understood directory structure URIs.
+Representations transfer JSON or XML to represent data objects and attributes.
+Messages use HTTP methods explicitly (for example, GET, POST, PUT, and DELETE).
+Stateless interactions store no client context on the server between requests. 
+State dependencies limit and restrict scalability. The client holds session state.
+
+*/
